c2f65b5db3d72e9e517c7a5ad94a65451d952f9f
Albert <albert.cieslak2@gmail.com>
2019-06-13 20:08:46 +0200

    print tree

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,26 @@ module Main where
 
 import Lib
 import TestGrammar
+import System.Environment (getArgs)
+import System.IO (openFile, hGetContents)
+import System.IO( IOMode( ReadMode ) )
+import ErrM( Err( Bad, Ok ) )
+import ParGrammar (myLexer, pProgram)
+import PrintGrammar (printTree)
+
+parseAndExecute :: String -> IO ()
+parseAndExecute s =
+  let result = pProgram (myLexer s)
+  in case result of
+    Bad s -> putStrLn "Parsing failed! :("
+    Ok tree -> putStrLn $ printTree tree
 
 main :: IO ()
-main = main2
+main = do
+  args <- getArgs
+  case args of
+      [fileName] -> do
+        handle <- openFile fileName ReadMode
+        contents <- hGetContents handle
+        parseAndExecute contents
+      _ -> putStrLn "Usage: ./interpreter program_to_run"
