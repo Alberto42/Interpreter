88c71e5e77c6b461e2c363568acb8a873105aaf4
Albert <albert.cieslak2@gmail.com>
2019-06-21 10:51:00 +0200

    split into good and bad

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import os
 import subprocess
 import shutil
 
-inputs = 'inputs'
+inputsGood = 'good'
+inputsBad = 'bad'
 outputs = 'outputs'
-directory = os.fsencode(inputs)
 
 folder = 'outputs'
 for the_file in os.listdir(folder):
@@ -16,11 +16,17 @@ for the_file in os.listdir(folder):
     except Exception as e:
         print(e)
 
-for file in os.listdir(directory):
-    filename_in = os.fsdecode(file)
-    filename_out = filename_in[:-2] + "out"
-    print(filename_in)
-    print(filename_out)
-    in_file = open(os.path.join(inputs, filename_in))
-    out_file = open(os.path.join(outputs, filename_out), mode='w')
-    subprocess.call(["./Interpreter2-exe", inputs + "/" + filename_in], stdout=out_file)
+
+def runAllInDirectory(inputs):
+    for file in os.listdir(os.fsencode(inputs)):
+        filename_in = os.fsdecode(file)
+        filename_out = filename_in[:-2] + "out"
+        print(filename_in)
+        print(filename_out)
+        in_file = open(os.path.join(inputs, filename_in))
+        out_file = open(os.path.join(outputs, filename_out), mode='w')
+        subprocess.call(["./Interpreter2-exe", inputs + "/" + filename_in], stdout=out_file)
+
+
+runAllInDirectory(inputsGood)
+runAllInDirectory(inputsBad)
